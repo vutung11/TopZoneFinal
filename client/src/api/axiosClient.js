86d950d63402e12e6807e002d22602f20efa43df
Tung Vu <tung.vu@mgiglobal.vn>
2023-03-10 15:39:48 +0700

    update link api

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:4000/api/',
+    baseURL: 'https://topzone.onrender.com/api/',
     withCredentials: true,
     headers: {
         'Content-type': 'application/json',
